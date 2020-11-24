89c5a4c28b4c5ec943d86fe8e79a0aeb0aed90f1
gaohan <gaohan97@foxmail.com>
2020-11-24 13:45:07 +0800

    负载均衡

--- consumer/src/main/java/com/gh/consumer/controller/ConsumerController.java
+++ consumer/src/main/java/com/gh/consumer/controller/ConsumerController.java
@@ -2,18 +2,20 @@ package com.gh.consumer.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class ManageController {
+public class ConsumerController {
     @Autowired
     RestTemplate restTemplate;
 
+
     @RequestMapping("/query")
     public String getEmpInfo(){
-        String info = restTemplate.getForObject("http://provider/emp", String.class);
-        return "消费者服务获取的信息: "+info;
+        String info = restTemplate.getForObject("http://provider/providerAPI", String.class);
+        return "消费者服务获取 " + info;
     }
 }
