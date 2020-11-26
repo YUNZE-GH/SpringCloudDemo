ba30d5cd3e0a385acfd101ebec0e6198b867c6cd
gaohan <gaohan97@foxmail.com>
2020-11-27 00:00:07 +0800

    mybatis-plus、swagger、log4j

--- provider2/src/main/java/com/gh/provider/Provider2Application.java
+++ provider2/src/main/java/com/gh/provider/Provider2Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ProviderApplication {
+public class Provider2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProviderApplication.class, args);
+		SpringApplication.run(Provider2Application.class, args);
 	}
 
 }
