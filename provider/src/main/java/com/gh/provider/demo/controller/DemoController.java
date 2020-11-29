7d66e72b622ad1d4ce371ae89d7b21a2e8ac06f0
gaohan <gaohan97@foxmail.com>
2020-11-29 23:12:16 +0800

    引用公共包jar，实现公共类及公共方法集中管理

--- provider/src/main/java/com/gh/provider/demo/controller/DemoController.java
+++ provider/src/main/java/com/gh/provider/demo/controller/DemoController.java
@@ -1,12 +1,12 @@
 package com.gh.provider.demo.controller;
 
 
+import com.gh.common.ResultData;
+import com.gh.common.enums.CodeEnum;
 import com.gh.provider.demo.entity.Demo;
 import com.gh.provider.demo.service.DemoService;
-import com.gh.provider.utils.CodeEnum;
-import com.gh.provider.utils.Result;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
  * @author gaohan
  * @since 2020-11-26
  */
-@RestController(value = "demo")
+@RestController
 public class DemoController {
 
     private final DemoService service;
@@ -29,10 +29,10 @@ public class DemoController {
         this.service = service;
     }
 
-    @GetMapping(value = "all")
-    public Result all() {
+    @RequestMapping(value = "/one")
+    public ResultData one() {
         Demo byId = service.getById("1");
-        return new Result(CodeEnum.SUCCESS.get(), byId, LocalDateTime.now().toString());
+        return new ResultData(CodeEnum.SUCCESS.get(), byId, LocalDateTime.now().toString());
     }
 }
 
