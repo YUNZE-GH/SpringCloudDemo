68b5c061f81264a7ac84b24523e7a4f62fbbdeff
gaohan <gaohan97@foxmail.com>
2020-12-13 09:23:55 +0800

    完善返回状态枚举类注释

--- common/src/main/java/com/gh/common/enums/CodeEnum.java
+++ common/src/main/java/com/gh/common/enums/CodeEnum.java
@@ -7,13 +7,13 @@ package com.gh.common.enums;
  */
 public enum CodeEnum {
     SUCCESS(0),
-    BUSINESS_ERROR(1),  // 业务异常
-    AUTHENTICATION_ERROR(2),    // 权限异常
-    SYSTEM_ERROR(3),    // 系统异常
-    PERMISSION_ERROR(4),// 允许异常
-    PARAMS_ERROR(5),   // 参数异常
-    FORMAT_ERROR(6),   // 格式化异常
-    OTHER_ERROR(9);    // 其他异常
+    BUSINESS_ERROR(1),      // 业务异常
+    AUTHENTICATION_ERROR(2),// 权限异常
+    SYSTEM_ERROR(3),        // 系统异常
+    PERMISSION_ERROR(4),    // 允许异常
+    PARAMS_ERROR(5),        // 参数异常
+    FORMAT_ERROR(6),        // 格式化异常
+    OTHER_ERROR(9);         // 其他异常
 
     private int value;
 
