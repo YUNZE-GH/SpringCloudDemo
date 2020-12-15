706d27dd0d2154ac60fe304ec591fac28556633f
gaohan <gaohan97@foxmail.com>
2020-12-15 17:59:14 +0800

    config-client

---
+++ config-client/src/main/java/com/gh/config/ConfigApplication.java
@@ -0,0 +1,24 @@
+package com.gh.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigApplication {
+
+	@Value("${foo}")
+	private String foo;
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApplication.class, args);
+	}
+
+	@RequestMapping(value = "get")
+	public String get(){
+		return foo;
+	}
+}
--- config/src/main/java/com/gh/config/ConfigApplication.java
+++ config/src/main/java/com/gh/config/ConfigApplication.java
@@ -2,16 +2,14 @@ package com.gh.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient //开启发现服务功能
+//@EnableDiscoveryClient //开启发现服务功能
 @EnableConfigServer
 public class ConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigApplication.class, args);
 	}
-
 }
