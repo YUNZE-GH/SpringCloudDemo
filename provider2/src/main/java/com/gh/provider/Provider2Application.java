3163cf63910857074bb4741081ca29d0b9c1459a
gaohan <gaohan97@foxmail.com>
2020-12-30 17:50:58 +0800

    开启多服务，测试负载均衡

--- provider2/src/main/java/com/gh/provider/Provider2Application.java
+++ provider2/src/main/java/com/gh/provider/Provider2Application.java
@@ -3,10 +3,10 @@ package com.gh.provider;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @MapperScan("com.gh.provider.modular.*.mapper")
 public class Provider2Application {
 
--- provider2/src/main/java/com/gh/provider/modular/demo/controller/DemoController.java
+++ provider2/src/main/java/com/gh/provider/modular/demo/controller/DemoController.java
@@ -5,12 +5,16 @@ import com.gh.common.toolsclass.ResultData;
 import com.gh.common.enums.CodeEnum;
 import com.gh.provider.modular.demo.entity.Demo;
 import com.gh.provider.modular.demo.service.DemoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -20,8 +24,11 @@ import java.time.LocalDateTime;
  * @author gaohan
  * @since 2020-11-26
  */
+@RefreshScope
 @RestController
 public class DemoController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Value("${server.port}")
     private String serverPort;
@@ -33,10 +40,25 @@ public class DemoController {
         this.service = service;
     }
 
-    @PostMapping(value = "/one")
-    public ResultData one() {
-        Demo byId = service.getById("1");
+    @PostMapping(value = "/one/{id}")
+    public ResultData one(@PathVariable("id") String id) {
+        logger.info("=====> /one");
+        Demo byId = service.getById(id);
         return new ResultData(CodeEnum.SUCCESS.get(), byId, serverPort, LocalDateTime.now().toString());
+    }
+
+    @PostMapping(value = "/all")
+    public ResultData all() {
+        List<Demo> list = service.list(null);
+        return new ResultData(CodeEnum.SUCCESS.get(), list, serverPort, LocalDateTime.now().toString());
+    }
+
+    @Value("${name}")
+    private String name;
+
+    @RequestMapping(value = "/test")
+    public String test(){
+        return name;
     }
 }
 
