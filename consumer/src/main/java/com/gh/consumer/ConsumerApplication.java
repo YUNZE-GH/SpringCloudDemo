6c54ca1453362cfe3c3dc32525a16e5b82e73499
gaohan <gaohan97@foxmail.com>
2020-12-30 17:50:17 +0800

    将消费者服务加入注册中心

--- consumer/src/main/java/com/gh/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/gh/consumer/ConsumerApplication.java
@@ -2,11 +2,11 @@ package com.gh.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableDiscoveryClient //开启发现服务功能
+//@EnableDiscoveryClient //开启发现服务功能
 @EnableFeignClients(basePackages = "com.gh.consumer.feign")
 @ComponentScan(basePackages = "com.gh.consumer.*")
 @SpringBootApplication
