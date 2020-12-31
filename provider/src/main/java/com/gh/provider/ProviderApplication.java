0a38e5cad180d1d88073f977356f795d9c32e23b
gaohan <gaohan97@foxmail.com>
2020-12-31 09:09:11 +0800

    @MapperScan注解添加多个扫描路径 @MapperScan({"com.gh.provider.modular.*.mapper", "com.gh.provider.*.mapper"})

--- provider/src/main/java/com/gh/provider/ProviderApplication.java
+++ provider/src/main/java/com/gh/provider/ProviderApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 //@EnableEurekaClient
-@MapperScan("com.gh.provider.modular.*.mapper")
+@MapperScan({"com.gh.provider.modular.*.mapper", "com.gh.provider.*.mapper"})
 public class ProviderApplication {
 
 	public static void main(String[] args) {
