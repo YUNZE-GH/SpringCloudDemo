65c8f2ab6f95ec754f246fcf1ef32e4038db1386
gaohan <gaohan97@foxmail.com>
2021-03-01 00:20:29 +0800

    webSocket集成实验

---
+++ consumer/src/main/java/com/gh/consumer/config/WebSocketStompConfig.java
@@ -0,0 +1,19 @@
+package com.gh.consumer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+/**
+ * @author gaohan
+ * @version 1.0
+ * @date 2021/2/28 22:49
+ */
+@Configuration
+public class WebSocketStompConfig {
+
+    @Bean
+    public ServerEndpointExporter serverEndpointExporter(){
+        return new ServerEndpointExporter();
+    }
+}
