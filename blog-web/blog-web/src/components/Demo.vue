4299dfe5a553e2b31984cde12ed1152d7172fcfd
gaohan <gaohan97@foxmail.com>
2021-03-05 08:46:57 +0800

    添加工具测试页面

---
+++ blog-web/blog-web/src/components/Demo.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="hello">
+        <el-button type="primary" @click="btnClick">主要按钮</el-button>
+        <br />
+        {{ info }}
+        <br />
+        <el-button type="primary" @click="btnClick2">主要按钮2</el-button>
+        <br />
+        <br />
+        <el-divider></el-divider>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                
+            };
+        },
+        methods: {
+            btnClick: function () {
+                let url1 = "https://api.coindesk.com/v1/bpi/currentprice.json";
+                let url2 = "/api/test";
+                this.$axios.get(url2).then((response) => {
+                    this.info = response;
+                    this.$message.success({
+                        message: "查询成功！",
+                        center: true
+                    });
+                });
+            },
+            btnClick2: function () {
+                this.$common.test();
+            }
+        },
+    };
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
