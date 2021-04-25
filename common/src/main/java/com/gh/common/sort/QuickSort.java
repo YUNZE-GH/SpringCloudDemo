6c17715cc813c2a29902208f2cb613ff0eb187e7
gaohan <gaohan97@foxmail.com>
2021-04-25 23:35:54 +0800

    快速排序+希尔排序

---
+++ common/src/main/java/com/gh/common/sort/QuickSort.java
@@ -0,0 +1,55 @@
+package com.gh.common.sort;
+
+/**
+ * @author gaohan
+ * @version 1.0
+ * @date 2021/4/25 21:49
+ */
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 4, 1, 6, 2, 5};
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSort(int[] arr, int leftIndex, int rightIndex) {
+        if (leftIndex >= rightIndex) {
+            return;
+        }
+
+        int left = leftIndex;
+        int right = rightIndex;
+        // 设置基准值，待排序的左侧第一个元素作为基准值
+        int key = arr[left];
+
+        // 从左右两边交替扫描，直到left = right
+        while (left < right) {
+            // 从右往左扫描，找到第一个比基准值小的元素，
+            while (left < right && arr[right] >= key) {
+                right--;
+            }
+            // 找到这种元素将arr[right]放入arr[left]中
+            arr[left] = arr[right];
+
+            while (left < right && arr[left] <= key) {
+                // 从左往右扫描，找到第一个比基准值大的元素
+                left++;
+            }
+            // 找到这种元素将arr[left]放入arr[right]中
+            arr[right] = arr[left];
+        }
+        // 基准值归位
+        arr[left] = key;
+
+        for (int a : arr) {
+            System.err.print(a + " ");
+        }
+        System.err.println();
+
+        // 对基准值左边的元素进行递归排序；leftIndex代表左区开始的索引，而此时的left为基准值的索引，所以left-1为左区结束的索引
+        quickSort(arr, leftIndex, left - 1);
+        // 对基准值右边的元素进行递归排序；此时right等于left，所以right此时也是基准值的索引,所以right+1为右区的开始索引，rightIndex为右区的结束索引
+        quickSort(arr, right + 1, rightIndex);
+    }
+
+}
---
+++ common/src/main/java/com/gh/common/sort/ShellSort.java
@@ -0,0 +1,42 @@
+package com.gh.common.sort;
+
+
+import java.util.Arrays;
+
+/**
+ * @author gaohan
+ * @version 1.0
+ * @date 2021/4/24 23:49
+ */
+public class ShellSort {
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 1, 3, 5, 4, 9, 7, 8};
+        shellSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    // 希尔排序
+    public static void shellSort(int[] arr) {
+        int count = 0;
+        // 挨个遍历步长，缩短步长，直到步长为零
+        for (int gap = arr.length / 2; gap > 0; gap = gap / 2) {
+            // 遍历所有的元素
+            for (int i = gap; i < arr.length; i++) {
+                // 遍历本组中所有的元素,从本组第一个元素开始
+                for (int index = i - gap; index >= 0; index -= gap) {   // index=index-gap本组前面的那个元素
+                    count++;
+                    // 如果当前元素大于加上步长之后的那个元素,（前面的比后面的大了）交换
+                    if (arr[index] > arr[index + gap]) {
+                        int temp = arr[index];
+                        arr[index] = arr[index + gap];
+                        arr[index + gap] = temp;
+//                        break;
+                    }
+
+                }
+            }
+        }
+        System.err.println(count);
+    }
+}
