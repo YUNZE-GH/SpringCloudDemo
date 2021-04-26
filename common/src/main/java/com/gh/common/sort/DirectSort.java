4d9de5bcc4f0ce6660439690e99edd87434c62a1
gaohan <gaohan97@foxmail.com>
2021-04-26 22:32:59 +0800

    添加直接排序法

---
+++ common/src/main/java/com/gh/common/sort/DirectSort.java
@@ -0,0 +1,31 @@
+package com.gh.common.sort;
+
+/**
+ * 直接排序
+ * @author gaohan
+ * @version 1.0
+ * @date 2021/4/26 22:25
+ */
+public class DirectSort {
+
+    public static void main(String[] args) {
+        int[] array = {1, 1, 2, 4, 7, 5};
+        directSort(array);
+    }
+
+    public static void directSort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+
+        for (int item : array) {
+            System.out.print(item + " ");
+        }
+    }
+}
