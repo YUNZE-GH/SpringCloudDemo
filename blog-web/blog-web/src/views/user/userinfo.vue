697b9fa4a7c9390000039ddc22f6f51dfc0c5c4e
gaohan <gaohan97@foxmail.com>
2021-06-18 19:52:06 +0800

    userinfo.vue

---
+++ blog-web/blog-web/src/views/user/userinfo.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        用户信息：{{userName}}
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                userName: "张三"
+            }
+        }
+    }
+
+</script>
+
+<style>
+</style>
