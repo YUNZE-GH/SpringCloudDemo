6a147a8bf9d7cce648c680e3f1d24985edb51a25
gaohan <gaohan97@foxmail.com>
2021-07-04 23:50:08 +0800

    1. 安装element-ui 2. 安装axios,并配置proxy代理，实现跨域请求

---
+++ blog-web/vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    devServer: {
+        open: true,
+        host: '0.0.0.0',    // 前端IP地址
+        port: 8080,         // 前端端口号
+        https: false,
+        // 配置不同的后台API地址
+        proxy: {
+            '/api': {   // 请求的时候用这个/api就可以,无需在加协议、IP和端口; 例： http://localhost:9011/test请求时，只需使用/api/test即可
+                target: `http://localhost:9011`,    // 接口服务IP地址
+                ws: true,
+                changOrigin: true,//允许跨域
+                pathRewrite: {
+                    '^/api': '/'
+                }
+            }
+        }
+    }
+}
