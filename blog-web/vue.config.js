cf4fe29b76d2ca1387d1a4e54370c73dc73f07c5
gaohan <gaohan97@foxmail.com>
2021-07-06 00:05:10 +0800

    实现在vue.config.js文件中使用env文件数据

--- blog-web/vue.config.js
+++ blog-web/vue.config.js
@@ -1,4 +1,7 @@
+let serverPath = process.env.VUE_APP_SERVER_PATH;
+
 module.exports = {
+    publicPath: "./",   // 打包相对路径
     devServer: {
         open: true,
         host: '0.0.0.0',    // 前端IP地址
@@ -7,7 +10,7 @@ module.exports = {
         // 配置不同的后台API地址
         proxy: {
             '/api': {   // 请求的时候用这个/api就可以,无需在加协议、IP和端口; 例： http://localhost:9011/test请求时，只需使用/api/test即可
-                target: `http://localhost:9011`,    // 接口服务IP地址
+                target: `${ serverPath }`,    // 接口服务IP地址
                 ws: true,
                 changOrigin: true,//允许跨域
                 pathRewrite: {
